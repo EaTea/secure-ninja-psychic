b4054e061354b3511bb62270445da5dfaa721033
Gwy <wying1211@gmail.com>
2013-10-15 13:21:11 +0800

    Added hashmap to swh

--- secure-ninja/src/edu/uwa/secure_ninja/swh/MySoftwareHouse.java
+++ secure-ninja/src/edu/uwa/secure_ninja/swh/MySoftwareHouse.java
@@ -8,70 +8,91 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+
+import edu.uwa.secure_ninja.developer.License;
 
 /**
  * @author wying1211
  *
  */
 public class MySoftwareHouse implements ISoftwareHouse {
-	public static void main(String[] args) {
-		MySoftwareHouse mswh = new MySoftwareHouse();
-		try {
-			ServerSocket server = new ServerSocket(1234);
-			mswh.generateLicenses(server.accept());
-			server.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	/** 
-	 */
-	public void generateLicenses(Socket connection) {
-		try {
-			DataInputStream in = new DataInputStream(
-					connection.getInputStream());
-			DataOutputStream out = new DataOutputStream(
-					connection.getOutputStream());
-			String library = in.readUTF();
-			int numLicenses = in.readInt();
-			String developerID = 
-					connection.getInetAddress().getHostName();
-			long time = System.currentTimeMillis();
-			String licence = library + numLicenses + developerID
-					+ time;
-			out.writeUTF(licence);
-			System.out.println(licence);
-			in.close();
-			out.close();
-			connection.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    /**
+     *
+     */
+    private HashMap<License, Integer> clients;
+    /**
+     * 
+     */
+    private ServerSocket server;
 
-	/** 
-	 */
-	public boolean verifyLicense(String license, String library,
-			String developerID) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    /**
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        MySoftwareHouse mswh = new MySoftwareHouse();
+        try {
+            mswh.server = new ServerSocket(1234);
+            mswh.generateLicenses(mswh.server.accept());
+            mswh.server.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-	/** 
-	 */
-	public void acceptLicenses(Socket connection) {
-		// TODO Auto-generated method stub
+    /**
+     *
+     */
+    public void generateLicenses(Socket connection) {
+        try {
+            DataInputStream in = new DataInputStream(
+                    connection.getInputStream());
+            DataOutputStream out = new DataOutputStream(
+                    connection.getOutputStream());
+            String library = in.readUTF();
+            int numLicenses = in.readInt();
+            String developerID =
+                    connection.getInetAddress().getHostName();
+            long time = System.currentTimeMillis();
+            String licence = library + numLicenses + developerID
+                    + time;
+            out.writeUTF(licence);
+            out.writeInt(numLicenses);
+//            System.out.println(licence);
+            License temp = new License(licence, server.getInetAddress(),
+                    library, connection.getInetAddress());
+            clients.put(temp, numLicenses);
+            in.close();
+            out.close();
+            connection.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-	}
+    /** 
+     */
+    public boolean verifyLicense(String license, String library,
+            String developerID) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	/** 
-	 */
-	public void sendLibraryFile(Socket connection, String libraryName) {
-		// TODO Auto-generated method stub
+    /** 
+     */
+    public void acceptLicenses(Socket connection) {
+        // TODO Auto-generated method stub
 
-	}
+    }
+
+    /** 
+     */
+    public void sendLibraryFile(Socket connection, String libraryName) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
