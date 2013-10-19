501b0e6d2898768024400d47dedee5b6f8c7ac6d
Edwin Tay <edwin.r.tay@gmail.com>
2013-10-19 19:38:05 +0800

    Adds byte[]->String method and removes stream closing

--- secure-ninja/src/edu/uwa/secure_ninja/NetworkUtilities.java
+++ secure-ninja/src/edu/uwa/secure_ninja/NetworkUtilities.java
@@ -97,7 +97,6 @@ public class NetworkUtilities {
                 success = false;
             }
 
-            closeSocketDataInputStream(inStream, connection);
             return success;
         }
         return false;
@@ -127,9 +126,20 @@ public class NetworkUtilities {
                 success = false;
             }
 
-            closeSocketDataOutputStream(outStream, connection);
             return success;
         }
         return false;
+    }
+
+    private static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        int v;
+        for ( int j = 0; j < bytes.length; j++ ) {
+            v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
     }
 }
