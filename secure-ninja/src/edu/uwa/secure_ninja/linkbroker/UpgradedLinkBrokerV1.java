31e196ccc26638f4922fce3c80acbca5b8ef9da8
Gwy <wying1211@gmail.com>
2013-10-19 19:31:44 +0800

    Upgraded the MyDev/MYLB/MYSWH to V1.

---
+++ secure-ninja/src/edu/uwa/secure_ninja/linkbroker/UpgradedLinkBrokerV1.java
@@ -0,0 +1,168 @@
+package edu.uwa.secure_ninja.linkbroker;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+import java.util.jar.Manifest;
+
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
+public class UpgradedLinkBrokerV1 {
+    private SSLServerSocketFactory sslSrvFact;
+
+    private SSLSocketFactory sslFact;
+
+    private int portNumber;
+
+    private SSLServerSocket server;
+
+    public void setPortNumber(int portNum) {
+        this.portNumber = portNum;
+    }
+
+    public int getPortNumber() {
+        return this.portNumber;
+    }
+
+    public UpgradedLinkBrokerV1(int portNum) {
+        this.setPortNumber(portNum);
+        sslSrvFact =
+                (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+        sslFact = (SSLSocketFactory) SSLSocketFactory.getDefault();
+    }
+
+
+    /**
+     * 
+     * @param connection
+     *            connection with developer
+     * @return
+     */
+    public void getFiles(SSLSocket connection) {
+        // TODO Auto-generated method stub
+        try {
+            DataInputStream in = new DataInputStream(
+                    connection.getInputStream());
+            DataOutputStream out = new DataOutputStream(
+                    connection.getOutputStream());
+            int JARNameLen = in.readInt();
+            String JARName = in.readUTF();
+            System.err.println(JARName);
+            FileOutputStream stream = new FileOutputStream(JARName + ".jar");
+            JarOutputStream jarOut =
+                    new JarOutputStream(stream, new Manifest());
+            int requests = in.readInt();
+            System.err.println(requests);
+            int count = 0;
+            SSLSocket[] swhConnections = new SSLSocket[requests];
+            DataInputStream[] swhIns = new DataInputStream[requests];
+            DataOutputStream[] swhOuts = new DataOutputStream[requests];
+            for (int i = 0; i < requests; i++) {
+                int swhIPLen = in.readInt();
+                String swhIP = in.readUTF();
+                System.err.println(swhIPLen + " " + swhIP);
+                swhConnections[i] =
+                        (SSLSocket) sslFact.createSocket
+                        (swhIP, 1778 /*(testing purpose)portNumber*/);
+                System.err.println(swhConnections[i].getPort());
+                swhIns[i] = new DataInputStream(
+                        swhConnections[i].getInputStream());
+                swhOuts[i] = new DataOutputStream(
+                        swhConnections[i].getOutputStream());
+                swhOuts[i].writeInt(in.readInt());
+                String license = in.readUTF();
+                System.err.println("<" + license + ">");
+                swhOuts[i].writeUTF(license);
+                long length = swhIns[i].readLong();
+                System.err.println(length);
+                if (length > -1) {
+                    count++;
+                    int len = swhIns[i].readInt();
+                    JarEntry jar = new JarEntry(swhIns[i].readUTF());
+                    jarOut.putNextEntry(jar);
+                }
+                for (long j = 0; j < length; j++) {
+                    jarOut.write(swhIns[i].read());
+                }
+            }
+            System.err.println(count);
+            if (count == requests) {
+                for (int i = 0; i < requests; i++) {
+                    swhOuts[i].writeBoolean(true); // success
+                    swhOuts[i].close();
+                    swhConnections[i].close();
+                }
+                out.writeInt(0);
+                // read class file from developer
+                int classes = in.readInt();
+                for (int i = 0; i < classes; i++) {
+                    int pathLen = in.readInt();
+                    jarOut.putNextEntry(new JarEntry(in.readUTF()));
+                    long size = in.readLong();
+                    int a;
+                    boolean success = true;
+                    for (long j = 0; j < size; j++) {
+                        a = in.read();
+                        if (a == -1) {
+                            success = false;
+                            break;
+                        } else {
+                            jarOut.write(a);
+                        }
+                    }
+                    // out.writeBoolean(success);
+                    if (!success) {
+                        return; // linking fail
+                    }
+                    // TODO jarOut.closeEntry();
+                }
+                jarOut.close();
+                File jar = new File(JARName + ".jar");
+                FileInputStream jarIn = new FileInputStream(jar);
+                out.writeLong(jar.length());
+                System.err.println(jar.length());
+                int a;
+                while ((a = jarIn.read()) != -1) {
+                    out.write(a);
+                }
+                jarIn.close();
+            } else {
+                out.writeInt(-1); // failed
+            }
+            out.close();
+            in.close();
+            jarOut.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        int port = Integer.parseInt(args[0]);
+        UpgradedLinkBrokerV1 bork = new UpgradedLinkBrokerV1(port);
+        System.err.println(bork.getPortNumber());
+        try {
+            bork.server = (SSLServerSocket) bork.sslSrvFact.createServerSocket(
+                    bork.getPortNumber(), 0 /* Java Implementation Specific*/ ,
+                                InetAddress.getLocalHost());
+            System.out.println("Link Broker "
+                    + bork.server.getInetAddress().getCanonicalHostName() + " "
+                    + bork.server.getLocalPort());
+            bork.getFiles((SSLSocket) bork.server.accept());
+            bork.server.close();
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
---
+++ secure-ninja/src/edu/uwa/secure_ninja/swh/UpgradedSoftWareHouseV1.java
@@ -0,0 +1,192 @@
+package edu.uwa.secure_ninja.swh;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.HashMap;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+
+import edu.uwa.secure_ninja.swh.UpgradedLicenseV1;
+
+public class UpgradedSoftWareHouseV1 {
+    /**
+    *
+    */
+    private HashMap<String, UpgradedLicenseV1> clientLicenses = new HashMap<String, UpgradedLicenseV1>();
+    /**
+    * 
+    */
+    private HashMap<String, File> libraries = new HashMap<String, File>();
+    /**
+    * 
+    */
+    private SSLServerSocketFactory sslSrvFact;
+
+    private SSLServerSocket server;
+
+    private int port;
+
+    public UpgradedSoftWareHouseV1(int port) {
+        sslSrvFact = (SSLServerSocketFactory) SSLServerSocketFactory
+                .getDefault();
+        setPort(port);
+    }
+
+    /**
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        int port = Integer.parseInt(args[0]);
+        UpgradedSoftWareHouseV1 mswh = new UpgradedSoftWareHouseV1(port);
+        for (int i = 1; i < args.length - 1; i += 2) {
+            mswh.addLibraryFile(args[i], args[i + 1]);
+        }
+        try {
+            mswh.server = (SSLServerSocket) mswh.sslSrvFact.createServerSocket(
+                    port, 0 /* Java Implementation Specific */,
+                    InetAddress.getLocalHost());
+            System.out.println("Software House: "
+                    + mswh.server.getInetAddress().getCanonicalHostName() + " "
+                    + mswh.server.getLocalPort());
+            for (String libName : mswh.libraries.keySet()) {
+                System.out.println("\t" + libName + " "
+                        + mswh.libraries.get(libName).getAbsolutePath());
+            }
+            mswh.generateLicenses((SSLSocket) mswh.server.accept());
+            mswh.acceptLicenses((SSLSocket) mswh.server.accept());
+            mswh.server.close(); // TODO: get rid of this?
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    /**
+    *
+    */
+    public void generateLicenses(SSLSocket connection) {
+        try {
+            DataInputStream in = new DataInputStream(
+                    connection.getInputStream());
+            DataOutputStream out = new DataOutputStream(
+                    connection.getOutputStream());
+            String library = in.readUTF();
+            if (libraries.containsKey(library)) {
+                int numLicenses = in.readInt();
+                String developerID = connection.getInetAddress().getHostName();
+                long time = System.currentTimeMillis();
+                String licence = library + numLicenses + developerID + time;
+                out.writeInt(licence.length());
+                out.writeUTF(licence);
+                // out.writeInt(numLicenses);
+                // System.out.println(licence);
+                UpgradedLicenseV1 temp = new UpgradedLicenseV1(licence,
+                        server.getInetAddress(), library, connection
+                                .getInetAddress().getHostName(), numLicenses);
+                clientLicenses.put(licence, temp);
+            } else {
+                out.writeInt(0); // 0 means no license
+            }
+            in.close();
+            out.close();
+            connection.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    /**
+    */
+    public boolean verifyLicense(String license) {
+        UpgradedLicenseV1 temp = clientLicenses.get(license);
+        System.err.println("[" + license + "]");
+        if (temp != null) {
+            if (temp.getNumberLicenses() > 0) {
+                return true;
+            } else {
+                clientLicenses.remove(temp);
+            }
+        }
+        return false;
+    }
+
+    /** 
+    */
+    public void acceptLicenses(SSLSocket connection) {
+        // TODO Auto-generated method stub
+        System.err.println("ME PORT NUMBER: " + connection.getLocalPort());
+        try {
+            DataInputStream in = new DataInputStream(
+                    connection.getInputStream());
+            int length = in.readInt();
+            String license = in.readUTF();
+            // accept it and send files;
+            if (verifyLicense(license)) {
+                UpgradedLicenseV1 temp = clientLicenses.get(license);
+                String libraryName = temp.getLibraryName();
+                sendLibraryFile(connection, libraryName);
+                if (in.readBoolean()) { // everything got linked up
+                    temp.decrementNumberLicenses();
+                }
+            } else {
+                // send -1 if declined
+                DataOutputStream out = new DataOutputStream(
+                        connection.getOutputStream());
+                out.writeLong(-1);
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 
+     * @param connection
+     * @param libraryName
+     */
+    public void sendLibraryFile(SSLSocket connection, String libraryName) {
+        try {
+            DataOutputStream out = new DataOutputStream(
+                    connection.getOutputStream());
+            File file = libraries.get(libraryName);
+            FileInputStream fileIn = new FileInputStream(file);
+            String path = file.getPath();
+            out.writeLong(file.length());
+            out.writeInt(path.length());
+            out.writeUTF(path);
+            int a;
+            while ((a = fileIn.read()) != -1) {
+                out.write(a);
+            }
+            fileIn.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 
+     * @param libraryName
+     * @param fileName
+     */
+    public void addLibraryFile(String libraryName, String fileName) {
+        libraries.put(libraryName, new File(fileName));
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    private void setPort(int port) {
+        this.port = port;
+    }
+
+}
