8142a6f541bbc6862651e48ff1605b3220c9654f
EaTea <edwin.r.tay@gmail.com>
2013-10-21 23:40:04 +0800

    Adds Client and Server modified code.

--- ssl-example/src/Client.java
+++ ssl-example/src/Client.java
@@ -1,30 +1,71 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
-
+import javax.net.ssl.TrustManagerFactory;
 
 public class Client {
 
-    private SSLSocket connection;
-    private SSLSocketFactory sslFact;
-    
-    public Client(String addr, int port) throws UnknownHostException, IOException {
-        sslFact = (SSLSocketFactory) SSLSocketFactory.getDefault();
-        connection = (SSLSocket) sslFact.createSocket(addr, port);
-    }
-    
-    public static void main(String[] args) throws IOException {
-        Client c = new Client(args[0], 8080);
-        DataInputStream dis = new DataInputStream(c.connection.getInputStream());
-        DataOutputStream dos = new DataOutputStream(c.connection.getOutputStream());
-        dos.writeUTF("Hello server!\n");
-        System.out.println(dis.readUTF());
-        dos.close();
-        dis.close();
-        c.connection.close();
-    }
+	private SSLSocket connection;
+	private SSLSocketFactory sslFact;
+
+	public Client(String addr, int port) throws UnknownHostException,
+	        IOException, NoSuchAlgorithmException, UnrecoverableKeyException,
+	        KeyManagementException, KeyStoreException, CertificateException {
+		sslFact = (SSLSocketFactory) getSSLSocketFactory();
+		connection = (SSLSocket) sslFact.createSocket(addr, port);
+	}
+
+	private SSLSocketFactory getSSLSocketFactory()
+	        throws NoSuchAlgorithmException, KeyStoreException,
+	        CertificateException, FileNotFoundException, IOException,
+	        UnrecoverableKeyException, KeyManagementException {
+		KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+		keyStore.load(new FileInputStream("./client-keystore.jks"),
+		        "cits3231".toCharArray());
+
+		KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+		trustStore.load(new FileInputStream("./client-truststore.jks"),
+		        "cits3231".toCharArray());
+
+		KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory
+		        .getDefaultAlgorithm());
+		kmf.init(keyStore, "cits3231".toCharArray());
+
+		TrustManagerFactory tmf = TrustManagerFactory
+		        .getInstance(TrustManagerFactory.getDefaultAlgorithm());
+		tmf.init(trustStore);
+
+		SSLContext ctx = SSLContext.getInstance("SSL");
+		ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+		return ctx.getSocketFactory();
+	}
+
+	public static void main(String[] args) throws IOException,
+	        NoSuchAlgorithmException, UnrecoverableKeyException,
+	        KeyManagementException, KeyStoreException, CertificateException {
+		Client c = new Client(args[0], 8080);
+		DataInputStream dis = new DataInputStream(c.connection.getInputStream());
+		DataOutputStream dos = new DataOutputStream(
+		        c.connection.getOutputStream());
+		dos.writeUTF("Hello server!\n");
+		System.out.println(dis.readUTF());
+		dos.close();
+		dis.close();
+		c.connection.close();
+	}
 }
--- ssl-example/src/Server.java
+++ ssl-example/src/Server.java
@@ -1,31 +1,72 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.Socket;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
 
 public class Server {
-    private SSLServerSocket server;
-    private SSLServerSocketFactory sslServFact;
-    
-    public Server(int port) throws IOException {
-        sslServFact = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
-        server = (SSLServerSocket) sslServFact.createServerSocket(port);
-    }
-    
-    public static void main(String[] args) throws IOException {
-        Server s = new Server(8080);
-        Socket sto = s.server.accept();
-        DataInputStream dis = new DataInputStream(sto.getInputStream());
-        DataOutputStream dos = new DataOutputStream(sto.getOutputStream());
-        String kk = dis.readUTF();
-        System.out.println("Client sent: " + kk);
-        String mk = "Hello client!";
-        dos.writeUTF(mk);
-        dis.close();
-        dos.close();
-        sto.close();
-    }
+	private SSLServerSocket server;
+	private SSLServerSocketFactory sslServFact;
+
+	public Server(int port) throws IOException, UnrecoverableKeyException,
+	        KeyManagementException, NoSuchAlgorithmException,
+	        KeyStoreException, CertificateException {
+		sslServFact = (SSLServerSocketFactory) getSSLServerSocketFactory();
+		server = (SSLServerSocket) sslServFact.createServerSocket(port);
+	}
+
+	private SSLServerSocketFactory getSSLServerSocketFactory()
+	        throws NoSuchAlgorithmException, KeyStoreException,
+	        CertificateException, FileNotFoundException, IOException,
+	        UnrecoverableKeyException, KeyManagementException {
+		KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+		keyStore.load(new FileInputStream("./server-keystore.jks"),
+		        "cits3231".toCharArray());
+		
+		KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+		trustStore.load(new FileInputStream("./server-truststore.jks"),
+		        "cits3231".toCharArray());
+
+		KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory
+		        .getDefaultAlgorithm());
+		kmf.init(keyStore, "cits3231".toCharArray());
+
+		TrustManagerFactory tmf = TrustManagerFactory
+		        .getInstance(TrustManagerFactory.getDefaultAlgorithm());
+		tmf.init(trustStore);
+
+		SSLContext ctx = SSLContext.getInstance("SSL");
+		ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+		return ctx.getServerSocketFactory();
+	}
+
+	public static void main(String[] args) throws IOException,
+	        UnrecoverableKeyException, KeyManagementException,
+	        NoSuchAlgorithmException, KeyStoreException, CertificateException {
+		Server s = new Server(8080);
+		Socket sto = s.server.accept();
+		DataInputStream dis = new DataInputStream(sto.getInputStream());
+		DataOutputStream dos = new DataOutputStream(sto.getOutputStream());
+		String kk = dis.readUTF();
+		System.out.println("Client sent: " + kk);
+		String mk = "Hello client!";
+		dos.writeUTF(mk);
+		dis.close();
+		dos.close();
+		sto.close();
+	}
 }
