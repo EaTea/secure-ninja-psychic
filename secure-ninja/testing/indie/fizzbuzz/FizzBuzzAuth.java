01b661f18a1d0afcd37fccb33cc7f969f02375d8
Edwin Tay <edwin.r.tay@gmail.com>
2013-10-24 17:14:19 +0800

    Added the authenticator for fizzBuzz

---
+++ secure-ninja/testing/indie/fizzbuzz/FizzBuzzAuth.java
@@ -0,0 +1,25 @@
+package indie.fizzbuzz;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class FizzBuzzAuth {
+
+    private static final Map<String, String> LICENSE_MAP = new HashMap<String, String>();
+    
+    private static final String PASSWORD = "";
+    
+    protected static void init() {
+        /* <----- LICENSE PUT HERE -----> */
+        /* <----- END LICENSE PUT HERE -----> */
+    }
+    
+    protected static String getLicense(String libName) {
+        return LICENSE_MAP.get(libName);
+    }
+    
+    protected static boolean checkPassword(String s) {
+        return s.equals(PASSWORD);
+    }
+    
+}
