9bc87c05eaca340567307eb7e917880b56be190f
Edwin Tay <edwin.r.tay@gmail.com>
2013-10-24 16:56:05 +0800

    Added the javadoc and some helper methods.

--- secure-ninja/src/snp/SecurityUtilities.java
+++ secure-ninja/src/snp/SecurityUtilities.java
@@ -17,8 +17,73 @@ import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManagerFactory;
 
+/**
+ * 
+ * @author Edwin Tay(20529864) && Wan Ying Goh(210784663)
+ *
+ */
 public class SecurityUtilities {
+    /**
+     *  The algorithm we used to generate secureRandomGenerator.
+     */
+    private static final String numberAlgo = "SHA1PRNG";
 
+    /**
+     * Private method to generate KeyStore.
+     * @param fis the file input stream
+     * @param password password for the keyStore
+     * @return a KeyStore if successful. Otherwise, null is returned.
+     */
+    private static KeyStore genKeyStore(FileInputStream fis, String password) {
+        KeyStore keyStore;
+        try {
+            keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            keyStore.load(fis, password.toCharArray());
+            fis.close();
+            return keyStore;
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("Error: the algorithm for the keystore could not be loaded");
+            e.printStackTrace();
+            return null;
+        } catch (CertificateException e) {
+            System.err.println("Error: could not process certificates");
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            System.err.println("Error: encountered I/O exception while loading key store");
+            e.printStackTrace();
+            return null;
+        } catch (KeyStoreException e) {
+            System.err.println("Error: no implementation of keystore available");
+            e.printStackTrace();
+            return null;
+        }
+        
+    }
+
+    /**
+     * Private method to generate SecureRandomNumberGenerator.
+     * @return a SecureRandomNumberGenerator if successful. Otherwise, null is returned.
+     */
+    private static SecureRandom genSecureRandomNumberGenerator() {
+        System.out.println("Generating a SHA1PRNG secure random number generator");
+        SecureRandom randomGen = null;
+        try {
+            randomGen = SecureRandom.getInstance(numberAlgo);
+            return randomGen;
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("Error: no implementation of RNG algorithm available");
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    /**
+     * Method to generate SSLSocketFactory.
+     * @param trustFile the filePath of the trustFile
+     * @param password the password for the keyStore
+     * @return SSLSocketFactory if successful. Otherwise, null is returned.
+     */
     public static SSLSocketFactory getSSLSocketFactory(String trustFile, String password) {
         System.out.println("Generating SSLSocketFactory with:");
         System.out.println("\ttrustFile: " + trustFile);
@@ -34,27 +99,8 @@ public class SecurityUtilities {
         }
         
         System.out.println("Loading trust store");
-        KeyStore trustStore = null;
-        try {
-            trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            trustStore.load(fis, password.toCharArray());
-            fis.close();
-
-        } catch (NoSuchAlgorithmException e) {
-            System.err.println("Error: the algorithm for the keystore could not be loaded");
-            e.printStackTrace();
-            return null;
-        } catch (CertificateException e) {
-            System.err.println("Error: could not process certificates");
-            e.printStackTrace();
-            return null;
-        } catch (IOException e) {
-            System.err.println("Error: encountered I/O exception while loading trust store");
-            e.printStackTrace();
-            return null;
-        } catch (KeyStoreException e) {
-            System.err.println("Error: no implementation of keystore available");
-            e.printStackTrace();
+        KeyStore trustStore = genKeyStore(fis, password);
+        if(trustStore == null) {
             return null;
         }
         
@@ -76,15 +122,11 @@ public class SecurityUtilities {
         
 
         System.out.println("Generating a SHA1PRNG secure random number generator");
-        SecureRandom randomGen = null;
-        try {
-            randomGen = SecureRandom.getInstance("SHA1PRNG");
-        } catch (NoSuchAlgorithmException e) {
-            System.err.println("Error: no implementation of RNG algorithm available");
-            e.printStackTrace();
+        SecureRandom randomGen = genSecureRandomNumberGenerator();
+        if(randomGen == null) {
             return null;
         }
-      
+        
         System.out.println("Generating a SSL context");
         try {
             // generate sslcontext
@@ -103,6 +145,12 @@ public class SecurityUtilities {
         }
     }
 
+    /**
+     * Method to generate SSLServerSocketFactory.
+     * @param keyFile the filePath of the keyFile
+     * @param password the password for the keyStore
+     * @return a SSLServerSocketFactory if successful. Otherwise, null is returned.
+     */
     public static SSLServerSocketFactory getSSLServerSocketFactory(String keyFile, String password) {
         System.out.println("Generating SSLServerSocketFactory with:");
         System.out.println("\tkeyFile: " + keyFile);
@@ -116,31 +164,14 @@ public class SecurityUtilities {
             return null;
         }
         
-        KeyStore keyStore = null;
-        
-        System.out.println("Loading KeyStore");
         // generate keystore
-        try {
-            keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            keyStore.load(fis, password.toCharArray());
-        } catch (KeyStoreException e) {
-            System.err.println("Error: no provider implements this KeyStore type");
-            e.printStackTrace();
-            return null;
-        } catch (NoSuchAlgorithmException e) {
-            System.err.println("Error: could not find KeyStore algorithm");
-            e.printStackTrace();
-            return null;
-        } catch (CertificateException e) {
-            System.err.println("Error: Certificate parsing encountered an error");
-            e.printStackTrace();
-            return null;
-        } catch (IOException e) {
-            System.err.println("Error: encountered I/O exception whilst loading KeyStore");
-            e.printStackTrace();
+        System.out.println("Loading KeyStore");
+        KeyStore keyStore = genKeyStore(fis, password);
+        if (keyStore == null) {
             return null;
         }
-        
+
+        //Generate keyManagerFactory
         System.out.println("Loading KeyManagerFactory");
         KeyManagerFactory kmf = null;
         try {
@@ -162,12 +193,8 @@ public class SecurityUtilities {
         }
 
         System.out.println("Generating a SHA1PRNG secure random number generator");
-        SecureRandom randomGen = null;
-        try {
-            randomGen = SecureRandom.getInstance("SHA1PRNG");
-        } catch (NoSuchAlgorithmException e) {
-            System.err.println("Error: no implementation of RNG algorithm available");
-            e.printStackTrace();
+        SecureRandom randomGen = genSecureRandomNumberGenerator();
+        if(randomGen == null) {
             return null;
         }
         
@@ -188,5 +215,4 @@ public class SecurityUtilities {
             return null;
         }
     }
-
 }
